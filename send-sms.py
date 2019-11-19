942bfa674bc1b4532e67fa2ac35ed0dcd173769a
kf7eel <57960556+kf7eel@users.noreply.github.com>
2019-11-19 12:04:53 -0800

    Add comments in file

--- send-sms.py
+++ send-sms.py
@@ -2,5 +2,7 @@
 import re, binascii, shark
 #shark.do_login()
 shark.do_checkauth()
+# First option is for calltype (0 - private, 1 - group), then format (0 - ETSI, 1 - UDP, 2 - UDP/Chinese).
+# Third option is the destination DMR ID, and the last is the actual message.
 shark.do_send_sms( '0', '1','9998', 'How long can this message be? 1234567' )
 
