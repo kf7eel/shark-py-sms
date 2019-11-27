ba6923e62ea89976b59d4984ae83cfc5b2f7243b
kf7eel <57960556+kf7eel@users.noreply.github.com>
2019-11-26 16:09:28 -0800

    Improvement
    Improved alerting capability and added documentation.
    Removed need for file in /tmp.

--- noaa.py
+++ noaa.py
@@ -1,19 +1,68 @@
 #!/usr/bin/python
-import re, binascii, shark
+
+# Script created by Eric - KF7EEL
+
+# This script will check for the specified alert types, then send
+# SMS to the configured DMR ID or Talk group.
+# Configure group of same codes to receive alerts for thos counties.
+# Set this on a cron job to check regularly.
+
+import re, binascii, shark, time
 from weatheralerts import WeatherAlerts
 
-alert_msg = '/tmp/.shark.alert_msg'
-f = open(alert_msg)
-lines = f.read() # lines()
+# Configure SMS here.
+# sms_type, 0 = private, 1 = group
+# sms_format, 0 = ETSI, 1 = UDP, 2 = UDP/Chinese
+# sms_dest, Talk group or DMR ID
+
+sms_type = "1"
+sms_format = "1"
+sms_dest = "9"
 
 
-nws = WeatherAlerts(samecodes=['015001']) #,'015003'])
+shark.do_checkauth()
+
+# Same codes for Chelan, Douglas, Grant, and Okanogan Counties
+nws = WeatherAlerts(samecodes=['053007','053017','053025','053047'])
+
+# Same codes for entire state
+#nws = WeatherAlerts(state='WA')
+
+# Filter for Severe alerts
 for alert in nws.alerts:
-#    if "Severe" or "Extreme" in alert.severity:
-    f_alert = open(alert_msg, 'w')
-    f_alert.write(alert.title)
-    f_alert.close
+    if "Severe" in alert.severity:
 # 1=Type, Group | 2=Format, UDP/Chinese | 9=Talkgroup | alert.title= message
-    shark.do_send_sms('1', '2', '9', alert.title)
-    print(alert.title)
-    print(alert.severity)
+        shark.do_send_sms(sms_type, sms_format, sms_dest, alert.title)
+        print(alert.title)
+        print(alert.severity)
+        print(alert.urgency)
+        print(alert.areadesc)
+        print(alert.samecodes)
+        print(alert.expiration)
+        print("\n")
+        time.sleep(1)
+
+# Filter for Moderate alerts
+    if "Moderate" in alert.severity:
+        shark.do_send_sms(sms_type, sms_format, sms_dest, alert.title)
+        print (alert.title)
+        print (alert.severity)
+        print(alert.urgency)
+        print(alert.areadesc)
+        print(alert.samecodes)
+        print(alert.expiration)
+        print("\n")
+        time.sleep(1)
+
+# Filter for Extreme alerts
+    if "Extreme" in alert.severity:
+        shark.do_send_sms(sms_type, sms_format, sms_dest, alert.title)
+        print (alert.title)
+        print (alert.severity)
+        print(alert.urgency)
+        print(alert.areadesc)
+        print(alert.samecodes)
+        print(alert.expiration)
+        print("\n")
+        time.sleep(1)
+
