18d8f6132645ea626a25af4edd261666318d2fdb
kf7eel <57960556+kf7eel@users.noreply.github.com>
2019-11-26 16:22:28 -0800

    Update modem change
    Updated to reflect change in shark.py.
    Now requires network or modem specified when sending sms.

--- send-sms.py
+++ send-sms.py
@@ -3,6 +3,6 @@ import re, binascii, shark
 #shark.do_login()
 shark.do_checkauth()
 # First option is for calltype (0 - private, 1 - group), then format (0 - ETSI, 1 - UDP, 2 - UDP/Chinese).
-# Third option is the destination DMR ID, and the last is the actual message.
-shark.do_send_sms( '0', '1','9998', 'How long can this message be? 1234567' )
+# Third option is the destination DMR ID, send to modem (1) or network (0), and the last is the actual message.
+shark.do_send_sms( '0', '1','9998', '1', 'How long can this message be? 1234567' )
 
